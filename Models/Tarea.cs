294578db42b26302aeb38212286a39c94f17f55a
Oswaldo Sierra <oswaldox199@gmail.com>
2026-06-22 16:24:53 -0500

    Agregar propiedades y anotaciones a las clases Categoria y Tarea, y configurar la conexión a la base de datos en Program.cs

--- Models/Tarea.cs
+++ Models/Tarea.cs
@@ -1,14 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace proyectoef.Models;
 
 public class Tarea
 {
+  [Key]
   public Guid TareaId { get; set; }
+
+  [ForeignKey("CategoriaId")]
   public Guid CategoriaId { get; set; }
+
+  [Required]
+  [MaxLength(200)]
   public string Titulo { set; get; }
   public string Descipcion { set; get; }
   public Prioridad PrioridadTarea { set; get; }
   public DateTime FechaCreacion { set; get; }
   public virtual Categoria Categoria { set; get; }
+
+  [NotMapped]
+  public string Resumen { set; get; }
 }
 
 public enum Prioridad
--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using proyectoef;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<TareasContext>(options =>
+    options.UseNpgsql("Host=127.0.0.1;Port=5432;Database=profectoef;Username=postgres;Password=root"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/dbconexion", async ([FromServices] TareasContext dbContext) =>
+{
+  dbContext.Database.EnsureCreated();
+  return Results.Ok("Base de datos en memoria:" + dbContext.Database.IsInMemory());
+});
 
 app.Run();
