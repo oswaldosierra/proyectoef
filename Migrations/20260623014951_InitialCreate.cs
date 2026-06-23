b09628b0300facac611c777cc486b5abfbc03562
Oswaldo Sierra <oswaldox199@gmail.com>
2026-06-22 20:59:35 -0500

    Agregar migraciones iniciales y actualizar el archivo de proyecto para incluir dependencias de diseño de Entity Framework

---
+++ Migrations/20260623014951_InitialCreate.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace proyectoef.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "categoria",
+                columns: table => new
+                {
+                    CategoriaId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Nombre = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false),
+                    Descripcion = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_categoria", x => x.CategoriaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tarea",
+                columns: table => new
+                {
+                    TareaId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CategoriaId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Titulo = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    Descipcion = table.Column<string>(type: "text", nullable: false),
+                    PrioridadTarea = table.Column<int>(type: "integer", nullable: false),
+                    FechaCreacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tarea", x => x.TareaId);
+                    table.ForeignKey(
+                        name: "FK_tarea_categoria_CategoriaId",
+                        column: x => x.CategoriaId,
+                        principalTable: "categoria",
+                        principalColumn: "CategoriaId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tarea_CategoriaId",
+                table: "tarea",
+                column: "CategoriaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tarea");
+
+            migrationBuilder.DropTable(
+                name: "categoria");
+        }
+    }
+}
